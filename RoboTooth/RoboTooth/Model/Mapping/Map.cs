3e487901516581c6f12dca0201abf61e00207ecf
Tadas.Paliulis <tadas.paliulis@gmail.com>
2019-08-10 15:34:46 +0100

    Initial commits of of Map And SensorHistory classes, currently just largely blank placeholders, not connected to anything

---
+++ RoboTooth/RoboTooth/Model/Mapping/Map.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoboTooth.Model.Mapping
+{
+    /// <summary>
+    /// Contains the current knowledge about the
+    /// world map
+    /// </summary>
+    public class Map
+    {
+        public List<Wall> _walls = new List<Wall>();
+    }
+
+    public class Wall
+    {
+        public Vector2 Position;
+
+        /// <summary>
+        /// How far the wall extends from its 'central'
+        /// position to the left, where left is determined with
+        /// the help of the FaceNormal.
+        /// </summary>
+        public double LengthLeft;
+
+        /// <summary>
+        /// How far the wall extends from its 'central'
+        /// position to the right, where right is determined with
+        /// the help of the FaceNormal.
+        /// </summary>
+        public double LengthRight;
+
+        /// <summary>
+        /// The orientation of the wall.
+        /// </summary>
+        public Vector2 FaceNormal;
+    }
+}
---
+++ RoboTooth/RoboTooth/Model/State/SensorHistory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoboTooth.Model.State
+{
+    /// <summary>
+    /// Represents a single sensor reading
+    /// </summary>
+    public interface ISensorReading
+    {
+        /// <summary>
+        /// The point in space where the measurement was made
+        /// </summary>
+        Vector2 MeasurementPoint { get; }
+
+        /// <summary>
+        /// The time when the measurement was made
+        /// </summary>
+        Duration MeasurementTime { get; }
+    }
+
+    /// <summary>
+    /// Keeps track of sensor recordings taken at any given point in time/space.
+    /// </summary>
+    public class SensorHistory<Reading> where Reading: ISensorReading 
+    {
+        public void HandleNewReadingAvailable(object sender, Reading newReading)
+        {
+            _readings.Add(newReading);
+        }
+
+        private List<Reading> _readings = new List<Reading>();
+    }
+}
