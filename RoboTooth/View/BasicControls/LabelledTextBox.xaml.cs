689f83d8984e41be1b1041f14ae7f0ebd3104735
Tadas <tadas.paliulis@gmail.com>
2020-05-02 15:15:59 +0100

    Created a LabelledTextBox user control combining a Label and a TextBox
    Replaced all instances of label + text box uses in the UI.
    Cleaned up The internal/target position display to use grids and layout
    better.

---
+++ RoboTooth/View/BasicControls/LabelledTextBox.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace RoboTooth.View.BasicControls
+{
+    /// <summary>
+    /// Interaction logic for LabelledTextBox.xaml
+    /// </summary>
+    public partial class LabelledTextBox : UserControl
+    {
+        public LabelledTextBox()
+        {
+            InitializeComponent();
+        }
+
+        #region Label Text Property property
+        public static readonly DependencyProperty LabelTextProperty =
+                DependencyProperty.Register(nameof(LabelText),
+                                            typeof(string),
+                                            typeof(LabelledTextBox),
+                                            new PropertyMetadata(null));
+
+        public string LabelText
+        {
+            get { return (string)GetValue(LabelTextProperty); }
+            set { SetValue(LabelTextProperty, value); }
+        }
+        #endregion
+
+        #region TextBox Content property
+        public static readonly DependencyProperty TextBoxContentProperty =
+                DependencyProperty.Register(nameof(TextBoxContent),
+                                            typeof(string),
+                                            typeof(LabelledTextBox),
+                                            new PropertyMetadata(null));
+
+        public string TextBoxContent
+        {
+            get { return (string)GetValue(TextBoxContentProperty); }
+            set { SetValue(TextBoxContentProperty, value); }
+        }
+        #endregion
+
+        #region Read Only  property
+        public static readonly DependencyProperty IsReadOnlyProperty =
+                DependencyProperty.Register(nameof(IsReadOnly),
+                                            typeof(bool),
+                                            typeof(LabelledTextBox),
+                                            new UIPropertyMetadata(false));
+
+        public bool IsReadOnly
+        {
+            get { return (bool)GetValue(IsReadOnlyProperty); }
+            set { SetValue(IsReadOnlyProperty, value); }
+        }
+        #endregion
+
+        #region TextBox Width  property
+        public static readonly DependencyProperty TextBoxWidthProperty =
+                DependencyProperty.Register(nameof(TextBoxWidth),
+                                            typeof(int),
+                                            typeof(LabelledTextBox),
+                                            new UIPropertyMetadata(20));
+
+        public int TextBoxWidth
+        {
+            get { return (int)GetValue(TextBoxWidthProperty); }
+            set { SetValue(TextBoxWidthProperty, value); }
+        }
+        #endregion
+    }
+}
